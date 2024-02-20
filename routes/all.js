6d3e507e2f5275043cd393496673c2ae3498813f
francesco-giorgione <francesco.giorgione01@gmail.com>
2024-02-20 14:05:30 +0100

    Bug fix

--- routes/all.js
+++ routes/all.js
@@ -7,9 +7,8 @@ router.get('/all/partite/in-vendita', async (req, res) => {
     let partiteInVendita = []
 
     for (const user of getAllMilkhubs()) {
-        let tmp = await ut.getProdottiProfilo(user.username, ut.getPartiteInVendita, ut.getPartita)
-        console.log(tmp)
-        partiteInVendita.concat(tmp)
+        let tmp = await ut.getProdottiProfilo(user.username, ut.getPartiteInVendita, ut.getPartita, true)
+        partiteInVendita = partiteInVendita.concat(tmp)
     }
 
     res.json(partiteInVendita)
@@ -19,9 +18,8 @@ router.get('/all/formaggi/in-vendita', async (req, res) => {
     let formaggiInVendita = []
 
     for (const user of getAllProducers()) {
-        let tmp = await ut.getProdottiProfilo(user.username, ut.getFormaggiInVendita, ut.getFormaggio)
-        console.log(tmp)
-        formaggiInVendita.concat(tmp)
+        let tmp = await ut.getProdottiProfilo(user.username, ut.getFormaggiInVendita, ut.getFormaggio, true)
+        formaggiInVendita = formaggiInVendita.concat(tmp)
     }
 
     res.json(formaggiInVendita)
